f001760977ddeda53545f7c2303dcd8b2c17b15a
DongHeon Lee <niche3600@gmail.com>
2023-01-20 16:10:10 +0900

    質問詳細ページに回答フォームを設置、回答のデータを保存する処理追加

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,10 +1,21 @@
 class AnswersController < ApplicationController
   # 回答の登録
   def create
+    # question_idパラメータを指定して質問データを取得する
+    @question = Question.find(params[:question_id])
+    # 質問データに　紐づく回答のデータを生成する
+    @question.answers.create(answer_params)
+    # 質問詳細画面にリダイレクトする
+    redirect_to question_path(@question)
   end
 
   # 回答の削除
   def destroy
   end
 
+  private
+
+  def answer_params
+    params.require(:answer).permit(:name, :content)
+  end
 end

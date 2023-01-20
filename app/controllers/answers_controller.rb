240d358c0085a302b519539153062e38b6fcf9f8
DongHeon Lee <niche3600@gmail.com>
2023-01-20 16:35:07 +0900

    質問詳細画面で回答の削除リンク追加、回答の削除処理を追加

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -11,6 +11,17 @@ class AnswersController < ApplicationController
 
   # 回答の削除
   def destroy
+    # question_idパラメータを指定して質問データを取得する
+    @question = Question.find(params[:question_id])
+    # idパラメータを指定して、質問データと紐づく回答データを取得する
+    @answer = @question.answers.find(params[:id])
+    # 回答を削除する
+    @answer.destroy
+    # statusに :see_otherを指定することでステータスコード303が使われる
+    # 新規リソースへの移動
+    # フォーム操作後に何かしらのURLに転送したい時に使われる
+    # see_otherをつけないと意図しない挙動にかる可能性がある
+    redirect_to @question, status: :see_other
   end
 
   private

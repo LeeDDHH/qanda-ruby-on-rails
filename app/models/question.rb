6c817b14681db4237258c2a0d770c86468a85ae6
DongHeon Lee <niche3600@gmail.com>
2023-01-20 14:54:11 +0900

    質問に対する回答のモデルとDBマイグレーションを定義
    - answerというモデルを生成
      - 1つのquestionに属する
    - questionモデルに追記
      - 1つのquestionに対して複数のanswerが連なる
      - questionを削除したら、それに依存するanswerも削除されるようにする

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,7 @@
 # 質問用のモデル
 class Question < ApplicationRecord
+  has_many :answers, dependent: :destroy
+
   validates :title, presence: true
   validates :name, presence: true
   validates :content, presence: true, length: { minimum: 5 }
